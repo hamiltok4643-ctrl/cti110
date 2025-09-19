6545e4725b88a230b46681b9466265012ede93ab
hamiltok4643-ctrl <hamiltok4643@student.faytechcc.edu>
2025-09-19 12:23:28 -0400

    BOO YEAHHHH

---
+++ P1HW1_HamiltonKristen.py
@@ -0,0 +1,22 @@
+#Kristen Hamilton
+#9/19/2025
+#P1HW1
+#Input and Output and with Integers
+
+
+print("----Calculating----")
+print()
+print()
+base_value = int(input("Enter an integer as the base value:"))
+exponent = int(input("Enter an integer as the exponent:"))
+print()
+print()
+print(base_value,"raised to the power of", exponent, "is", base_value**exponent,"!!") 
+print()
+print()
+print("-----Addition and Subtractio-----")
+my_num = int(input("Enter a starting integer:"))
+add = int(input("Enter an integer to add:"))
+subtract = int(input("Enter an integer to subtract:"))
+print()
+print(my_num,"+", add, "-", subtract, "is equal to", my_num + add - subtract) 
