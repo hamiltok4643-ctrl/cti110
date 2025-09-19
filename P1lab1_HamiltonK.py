4f79d5c807e087271208a2aee3b63348f4aceeea
hamiltok4643-ctrl <hamiltok4643@student.faytechcc.edu>
2025-09-19 10:58:50 -0400

    Finish My Python P1lab1

---
+++ P1lab1_HamiltonK.py
@@ -0,0 +1,13 @@
+#Kristen Hamilton
+#9/19/2025
+#P1lab1
+#Input and Output
+
+
+#The input function always saves the date as a string by default
+first_name = input("enter your first name:")
+last_name = input("enter your last name:")
+print()
+print( "Hello", first_name, last_name + "!" "Welcome to CTI to CTI-110.")
+print()
+print(type(first_name))
